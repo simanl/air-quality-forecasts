15fe112ba218ad0ca7a8961c183bbdfd22ed61e1
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2015-10-16 15:16:26 -0500

    Script para descargar archivos de entrada de ejemplo (ciclos) en un folder temporal

---
+++ tmp/download-sample-data.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+wget -O sample-data-csvs.tar.bz2 \
+  "https://www.dropbox.com/s/39d36j27a0dgssu/sample-data-csvs.tar.bz2?dl=0" \
+  && tar xvf sample-data-csvs.tar.bz2 --no-same-owner
